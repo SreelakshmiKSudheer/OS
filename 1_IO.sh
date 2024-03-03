768fac77a5f027b6c31d9b863a1b6f06b943c796
Sreelakshmi K <119166968+SreelakshmiKSudheer@users.noreply.github.com>
2024-03-03 14:47:15 +0530

    Update 1_IO.sh

--- 1_IO.sh
+++ 1_IO.sh
@@ -1,4 +1,12 @@
+#!/bin/sh
 # Basic IO
 echo "What is your name:"
 read name
+echo "How old are you:"
+read age
+
+current_year=2024
+readonly current_year   # makes the var a read only done, can't be ovewritten
+yob=$((current_year-age))
 echo "Hi, $name"
+echo "You are born in $yob"
