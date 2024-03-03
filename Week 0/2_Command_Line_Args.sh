5386ce5dc29ff2a259e79a642f884c27079bca64
Sreelakshmi K <119166968+SreelakshmiKSudheer@users.noreply.github.com>
2024-03-03 17:50:23 +0530

    Update 2_Command_Line_Args.sh

--- Week 0/2_Command_Line_Args.sh
+++ Week 0/2_Command_Line_Args.sh
@@ -1,1 +1,15 @@
+#!/bin/sh
+# Command line args
+# the "$*" special parameter takes the entire list as one argument with spaces between
+# "$@" special parameter takes the entire list and separates it into separate arguments.
+echo "File name: $0"
+echo "First parameter: $1"
+echo "Second parameter: $2"
+echo "Quaoted values: $@"
+echo "Quoted values: $*"
+echo "Total no. parameter: $#"
 
+for TOKEN in $*
+do
+    echo $TOKEN
+done
