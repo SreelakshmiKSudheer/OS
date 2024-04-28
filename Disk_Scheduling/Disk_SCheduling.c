0fb6befb3b47b4e29d5af314ff74ee2e217b5027
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-04-28 08:09:25 +0530

    Disk_Scheduling : C-LOOK

--- Disk_Scheduling/Disk_SCheduling.c
+++ Disk_Scheduling/Disk_SCheduling.c
@@ -178,6 +178,37 @@ void look(int r[],int n,int head)
     printf("\nAvg seek time: %.2f\n\n",(float)seek_sum/n);
 }
 
+void clook(int r[],int n,int head)
+{
+    int i,seek_sum = 0,index = 0;
+    sort(r,n);
+
+    printf("\n\nC-SCAN\n");
+    printf("Accessing order of data: \n");
+    
+    seek_sum += abs(r[n-1] - head);
+    seek_sum += r[n-1] - r[0];
+
+    for(i = 0; i < n; i++)
+    {
+        if(r[i] < head)
+            continue;
+        index++;
+        printf("%d ",r[i]);
+        
+    }
+    index = n - index;
+    seek_sum += r[index - 1] - r[0];
+
+    for(i = 0; i < index; i++)
+    {
+        printf("%d ",r[i]);
+    }
+
+    printf("\nTotal seek time: %d",seek_sum);
+    printf("\nAvg seek time: %.2f\n\n",(float)seek_sum/n);
+}
+
 int main()
 {
     int N,i,head,limit;
@@ -204,6 +235,6 @@ int main()
     scan(r,n,head,limit);
     cscan(r,n,head,limit);
     look(r,n,head);
-    //clook(r,n,head);
+    clook(r,n,head);
 
 }
