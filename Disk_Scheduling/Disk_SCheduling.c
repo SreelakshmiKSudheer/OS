e7d4327c815a1768c2eb209818206d837f644b67
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-04-28 06:37:57 +0530

    Disk_Scheduling : FIFO

---
+++ Disk_Scheduling/Disk_SCheduling.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+
+void fcfs(int r[],int n,int head)
+{
+    int i,seek_sum = 0;
+
+    printf("\n\nFCFS\n");
+    printf("Accessing order of data: \n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ",r[i]);
+        seek_sum += abs(head - r[i]);
+        head = r[i];
+    }
+    printf("\nTotal seek time: %d",seek_sum);
+    printf("\nAvg seek time: %.2f\n\n",(float)seek_sum/n);
+}
+
+int main()
+{
+    int N,i,head;
+
+    printf("Enter the no. of requests: ");
+    scanf("%d",&N);
+
+    const int n = N;
+    int r[n];
+    printf("Enter the positions of the requested data: \n");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d",&r[i]);
+    }
+
+    printf("Enter the current position of disk head: ");
+    scanf("%d",&head);
+
+    fcfs(r,n,head);
+    //sstf(r,n,head);
+    //scan(r,n,head);
+    //cscan(r,n,head);
+    //look(r,n,head);
+    //clook(r,n,head);
+
+}
