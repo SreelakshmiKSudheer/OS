9c42dba192f789dd7e479fd1f6acfe673ed60f44
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-05-07 17:12:49 +0530

    correction

--- 8_Memory Management.c
+++ 8_Memory Management.c
@@ -4,6 +4,39 @@
 
 int partition[SIZE] = {600,320,245,125,575};
 
+void display(int external,int internal,int rec[],int order[])
+{
+        int i;
+
+        printf("Memory Allocation:\n");
+        for(i = 0; i < SIZE; i++)
+        {
+                if(rec[i] != 0)
+                        printf("%d KB partition is allocated to process P%d\n", partition[i],order[i]);
+        }
+
+        if(internal == 0 && external == 0)
+                printf("No fragmentation\n");
+        else if(internal != 0 && external == 0)
+        {
+                printf("Memory wastage due to internal fragmentation: %d\n",internal);
+                printf("No external fragmentation\n");
+        }
+        else if(internal == 0 && external != 0)
+        {
+                printf("Memory wastage due to external fragmentation: %d\n",external);
+                printf("No internal fragmentation\n");
+        }
+        else
+        {
+                printf("Memory wastage due to internal fragmentation: %d\n",internal);
+                printf("Memory wastage due to external fragmentation: %d\n",external);
+
+        }
+
+        printf("\n----------------------------------------------------------\n");
+}
+
 void ff(int mr[],int N)
 {
         int internal = 0, external = 0;
@@ -34,73 +67,22 @@ void ff(int mr[],int N)
         }
 
         printf("\nFirst Fit\n");
-        printf("Memory Allocation:\n");
-        for(i = 0; i < SIZE; i++)
-        {
-                if(rec[i] != 0)
-                        printf("%d KB partition is allocated to process P%d\n", partition[i],order[i]);
-        }
-
-        if(internal == 0 && external == 0)
-                printf("No fragmentation\n");
-        else if(internal != 0 && external == 0)
-        {
-                printf("Memory wastage due to internal fragmentation: %d\n",internal);
-                printf("No external fragmentation\n");
-        }
-
-       }
-        else if(internal == 0 && external != 0)
-        {
-                printf("Memory wastage due to external fragmentation: %d\n",external);
-                printf("No internal fragmentation\n");
-        }
-        else
-        {
-                printf("Memory wastage due to internal fragmentation: %d\n",internal);
-                printf("Memory wastage due to external fragmentation: %d\n",external);
-
-        }
-        printf("\n----------------------------------------------------------\n");
-
+        display(external,internal,rec,order);
+        
 }
-
 
 void bf(int mr[],int N)
 {
         int internal = 0, external = 0,min;
-int main()
-{
-        int i,N;
-        printf("Memory partition is given below:\n| ");
-
-        for(i = 0; i < SIZE; i++)
-                printf("%8d KB| ",partition[i]);
-        printf("\n");
-
-        printf("How many processes? ");
-        scanf("%d",&N);
-
-        const int n = N;
-        int mr[n];
-
-        printf("Enter the memory requirements of each process: \n");
-
-        for(i = 0; i < n; i++)
-                scanf("%d",&mr[i]);
-
-        ff(mr,n);
-        bf(mr,n);
-        wf(mr,n);
-}
-
 
         const int n = N;
         int i, j, rec[SIZE], order[n];
 
         for(i = 0; i < SIZE; i++)
+        {
                 rec[i] = 0;
-
+                order[i] = 0;
+        }
         for(i = 0; i < n; i++)
         {
                 min = -1;
@@ -127,38 +109,7 @@ int main()
         }
 
         printf("\nBest Fit\n");
-
-
-
-
-        printf("Memory Allocation:\n");
-        for(i = 0; i < SIZE; i++)
-        {
-                if(rec[i] != 0)
-                        printf("%d KB partition is allocated to process P%d\n", partition[i],order[i]);
-        }
-
-        if(internal == 0 && external == 0)
-                printf("No fragmentation\n");
-        else if(internal != 0 && external == 0)
-        {
-                printf("Memory wastage due to internal fragmentation: %d\n",internal);
-                printf("No external fragmentation\n");
-        }
-        else if(internal == 0 && external != 0)
-
-        {
-
-                printf("Memory wastage due to external fragmentation: %d\n",external);
-                printf("No internal fragmentation\n");
-        }
-        else
-        {
-                printf("Memory wastage due to internal fragmentation: %d\n",internal);
-                printf("Memory wastage due to external fragmentation: %d\n",external);
-
-        }
-        printf("\n----------------------------------------------------------\n");
+        display(external,internal,rec,order);
 
 }
 
@@ -213,46 +164,7 @@ void wf(int mr[],int N)
         }
 
         printf("\nWorst Fit\n");
-
-
-        printf("Memory Allocation:\n");
-        for(i = 0; i < SIZE; i++)
-        {
-                if(rec[i] != 0)
-
-                       external += partition[i];
-        }
-
-        printf("\nWorst Fit\n");
-
-
-        printf("Memory Allocation:\n");
-        for(i = 0; i < SIZE; i++)
-        {
-                if(rec[i] != 0)
-                        printf("%d KB partition is allocated to process P%d\n", partition[i],order[i]);
-        }
-
-        if(internal == 0 && external == 0)
-                printf("No fragmentation\n");
-        else if(internal != 0 && external == 0)
-        {
-                printf("Memory wastage due to internal fragmentation: %d\n",internal);
-                printf("No external fragmentation\n");
-        }
-        else if(internal == 0 && external != 0)
-
-        {
-                printf("Memory wastage due to external fragmentation: %d\n",external);
-                printf("No internal fragmentation\n");
-        }
-        else
-        {
-                printf("Memory wastage due to internal fragmentation: %d\n",internal);
-                printf("Memory wastage due to external fragmentation: %d\n",external);
-
-        }
-        printf("\n----------------------------------------------------------\n");
+        display(external,internal,rec,order);
 
 }
 
