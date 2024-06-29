6f47349106f6f598e385878b61665bd745116211
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-06-29 09:41:11 +0530

    Divide into functions

--- ProducerConsumer.c
+++ ProducerConsumer.c
@@ -20,11 +20,48 @@ void sig(int *semaphore)
     (*semaphore)++;
 }
 
+void  pro()
+{
+    if(in == -1)
+        {
+            in = 0;
+            out = 0;
+        }
+        else
+            in = (in + 1) % MAX;
+}
 
+void take()
+{
+    out = (out+1) % MAX;
+    if(out == (in + 1) % MAX)
+    {
+        in = out = -1;
+    }
+}
+
+void bufferDisplay()
+{
+    int j;
+
+    printf("Buffer: [");
+    j = out;
+    if(out != -1 && in != -1)
+    {
+        while(j < in)
+        {
+            printf("%d ",buffer[j]);
+            j = (j+1) % MAX;
+        }
+        printf("%d]\n",buffer[in]);
+    }
+    else
+        printf("Buffer Empty\n");
+}
 
 void *produce(void *arg)
 {
-    int i,j;
+    int i;
     for(i = 0; i < 5; i++)
     {
         next_produced++;        // next item is produced in next_produced
@@ -32,25 +69,12 @@ void *produce(void *arg)
         wait(&empty);
         wait(&semaphore);
     
-        if(in == -1)
-        {
-            in = 0;
-            out = 0;
-        }
-        else
-            in = (in + 1) % MAX;
+        pro();
         buffer[in] = next_produced;
 
         printf("Producer: produced %d\n",buffer[in]);
-        printf("Buffer: [");
-        j = out;
-        while(j < in)
-        {
-            printf("%d ",buffer[j]);
-            j = (j+1) % MAX;
-        }
-        printf("%d]\n",buffer[in]);
-
+        bufferDisplay();
+        
         sig(&semaphore);
         sig(&full);
 
@@ -70,19 +94,8 @@ void *consume()
         
         next_consumed = buffer[out];
         printf("Consumer: consumed %d\n",buffer[out]);
-        out = (out+1) % MAX;
-        if(out == (in + 1) % MAX)
-        {
-            in = out = -1;
-        }
-        printf("Buffer: [");
-        j = out;
-        while(j < in)
-        {
-            printf("%d ",buffer[j]);
-            j = (j+1) % MAX;
-        }
-        printf("%d]\n",buffer[in]);
+        take();
+        bufferDisplay();
         
         sig(&semaphore);
         sig(&empty);
