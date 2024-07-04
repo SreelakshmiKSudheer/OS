1b0e35c9816375707f2408e32ef16c897c0a9c09
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-07-04 22:51:53 +0530

    writer

--- Reader-Writer.c
+++ Reader-Writer.c
@@ -35,3 +35,33 @@ void writer(void *arg)
     }
 }
 
+void reader(void *arg)
+{
+    int reader_id = *((int*)arg);       // reader_id given as the function parameter
+
+    while(1)
+    {
+        wait_s(&mutex);                 // wait if any other readers are updating read_count
+        read_count++;                   // increment read_count
+        
+        /*if this is the first reader(read_count = 1), 
+            request lock 
+                if rw_mutex > 0 meaning some writer is currently writing (this is the first reader, so no other readers)
+                    - request lock is held, waits till access allowed
+        */
+        if(read_count == 1)
+            wait_s(&rw_mutex);
+        signal_s(&mutex);
+
+        printf("Reader %d: reads %d\n",reader_id,shr);
+
+        wait_s(&mutex);         // if some reader is accessing read_count, wait
+        read_count--;           // decrement read_count
+        /*
+        if this is the last reader it should ensure that any other writer who is waiting to write should be allowed enter critical section*/
+        if(read_count == 0)
+            signal_s(&rw_mutex);
+        signal_s(&mutex);       // read_count made accessible to other readers
+        sleep(1);
+    }
+}
