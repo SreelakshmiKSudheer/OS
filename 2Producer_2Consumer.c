1a59aa7bdeaa32de8fe3bd9a2bb1300692a95737
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-07-07 20:41:51 +0530

    completed correction - 3 mutex

--- 2Producer_2Consumer.c
+++ 2Producer_2Consumer.c
@@ -6,7 +6,7 @@
 #define MAX 5
 
 int buffer[MAX];
-int empty = MAX, full = 0, semaphore = 1;
+int empty = MAX, full = 0, pmutex = 1, pc_mutex = 1, cmutex = 1;
 int in = -1, out = -1;
 int next_produced = 0, next_consumed;
 
@@ -39,3 +39,95 @@ void bufferDisplay()
         printf("Buffer empty\n");
 }
 
+void inUpdate()
+{
+    if(in == -1)
+        out = 0;
+    in = (in+1) % MAX;
+}
+
+void outUpdate()
+{
+    out = (out + 1) % MAX;
+    if(out == (in+1) % MAX)
+        out = in = -1;
+}
+
+void *produce(void *arg)
+{
+    int id = *((int*)arg);
+    int item;
+    if(id == 1)
+        item = 1;
+    else
+        item = 2;
+
+    while(1)
+    {
+        wait_s(&empty);
+        wait_s(&pc_mutex);
+        wait_s(&pmutex);
+
+        inUpdate();
+        buffer[in] = item;
+        printf("Producer %d: produced item %d\n", id,buffer[in]);
+        bufferDisplay();
+        //sleep(1);
+        item += 2;
+        signal_s(&pmutex);
+        signal_s(&pc_mutex);
+        signal_s(&full);
+        sleep(1);
+    }
+    pthread_exit(NULL);
+}
+
+void *consume(void *arg)
+{
+    int id = *((int*)arg);
+
+    while(1)
+    {
+        wait_s(&full);
+        wait_s(&pc_mutex);
+        wait_s(&cmutex);
+
+        printf("Consumer %d: consumes %d\n",id,buffer[out]);
+        outUpdate();
+        bufferDisplay();
+        
+        signal_s(&cmutex);
+        signal_s(&pc_mutex);
+        signal_s(&empty);
+        sleep(2);
+    }
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    int pid[2],cid[2],i;
+    pthread_t producer[2],consumer[2];
+
+    for(i = 0; i < 2; i++)
+    {
+        pid[i] = i+1;
+        pthread_create(&producer[i],NULL,produce,&pid[i]);
+    }
+
+    for(i = 0; i < 2; i++)
+    {
+        cid[i] = i+1;
+        pthread_create(&consumer[i],NULL,consume,&cid[i]);
+    }
+
+    for(i = 0; i < 2; i++)
+    {
+        pthread_join(producer[i],NULL);
+    }
+
+    for(i = 0; i < 2; i++)
+    {
+        pthread_join(consumer[i],NULL);
+    }
+}
