20a2f2bbffdd555127bbfba4c22e136e6bd07f66
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-07-11 19:34:25 +0530

    calculator

--- cycle1/Calculator.sh
+++ cycle1/Calculator.sh
@@ -1,44 +1,37 @@
 #!/bin/sh
 
 continue=1
-
 while [ $continue -eq 1 ]
 do
-    echo -n "Num1: "
-    read a
-    echo -n "Num2: "
-    read b
-    echo "Select operation:"
-    echo "1. Addition"
-    echo "2. Subtraction"
-    echo "3. Multiplication"
-    echo "4. Division"
-    echo -n "Operation: "
-    read op
-
-    if [ $op -eq 1 ]; then
-        result=$((a + b))
-        echo "Result: $result"
-    elif [ $op -eq 2 ]; then
-        result=$((a - b))
-        echo "Result: $result"
-    elif [ $op -eq 3 ]; then
-        result=$((a * b))
-        echo "Result: $result"
-    elif [ $op -eq 4 ]; then
-        if [ $b -ne 0 ]; then
-            result=$((a / b))
-            echo "Result: $result"
-        else
-            echo "Division by zero is not allowed."
-        fi
-    else
-        echo "Invalid operation. Please select between 1-4."
-    fi
-
-    echo -n "Continue? (1 for yes, 0 for no): "
-    read continue
-    if [ $continue -eq 0 ]; then
-        exit 0
-    fi
+read a
+read b
+read op
+case $op in
+1)
+result=$((a+b))
+echo $result
+;;
+2)
+result=$((a-b))
+echo $result
+;;
+3)
+result=$((a*b))
+echo $result
+;;
+4)
+if [ $b -ne 0 ]; then
+result=$((a/b))
+echo $result
+else
+echo "zero division error"
+fi
+;;
+*)
+echo "Invalid operation"
+esac
+read continue
+if [ $continue -eq 0 ]; then
+exit
+fi
 done
